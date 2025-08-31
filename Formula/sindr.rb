f425056b3a273c7f7b1eac098d3acadf8c492587
Martin Wohlfart <github@wohlf.art>
2025-08-31 12:06:45 +0200

    Bump to v0.0.8

--- Formula/sindr.rb
+++ Formula/sindr.rb
@@ -1,8 +1,8 @@
 class Sindr < Formula
   desc "Project-specific commands as a CLI"
   homepage "https://github.com/mbark/sindr"
-  url "https://github.com/mbark/sindr/archive/refs/tags/v0.0.7.tar.gz"
-  sha256 "4227cbdd40614e6e0cc6338435e80945ab35044ce17960a86b2e8c7c0f4f5dc7"
+  url "https://github.com/mbark/sindr/archive/refs/tags/v0.0.8.tar.gz"
+  sha256 "7e55e0738902df291eeaf497c57ebdf5bbe2e785aded5fe760cafcc094aa5431"
   license "MIT"
 
   depends_on "go" => :build
